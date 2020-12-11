b69445e49e946e8a0506a4b3ecb0579066feca9d
Jedrzej Szadejko <jendziu@gmail.com>
2020-12-11 23:38:03 +0100

    Added deletion, modification and creation to solution and exercises

--- src/MB.Backend/exercises/urls.py
+++ src/MB.Backend/exercises/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("<int:pk>/", views.ExerciseDetails.as_view()),
+    path("<int:pk>/solution/", views.SolutionDetail.as_view()),
+    path("<int:pk>/solution/add/", views.CreateSolution.as_view()),
     path("create/", views.CreateExercise.as_view()),
     path("list/", views.ExerciseList.as_view())
 ]
