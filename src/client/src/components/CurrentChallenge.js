141d50c6e9ca40cb54a3bc99147cd9e6d1691bfa
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-12-12 15:07:08 +0100

    added feedback form

--- src/client/src/components/CurrentChallenge.js
+++ src/client/src/components/CurrentChallenge.js
@@ -19,6 +19,7 @@ import React, { forwardRef, useEffect, useState } from "react";
 import { Close, StarBorder } from "@material-ui/icons";
 import Rating from "@material-ui/lab/Rating";
 import { mapDiff } from "../utils/helpers";
+import Feedback from "./Feedback";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -40,6 +41,7 @@ const CurrentChallenge = ({
   history,
 }) => {
   const [selected, setSelected] = useState(null);
+  const [openFeedback, setOpenFeedback] = useState(false);
   const classes = useStyles();
   console.log(selected);
 
@@ -56,7 +58,6 @@ const CurrentChallenge = ({
 
   const handleClose = () => {
     history.push("/");
-    setOpen(false);
   };
 
   // const {id} = params;
@@ -75,56 +76,66 @@ const CurrentChallenge = ({
   const solutions = [solution_a, solution_b, solution_c, solution_d];
 
   return (
-    <Dialog fullScreen open={open} onClose={handleClose}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={handleClose}
-            aria-label="close"
+    <>
+      <Dialog fullScreen open={open} onClose={handleClose}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <Close />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              {name}
+            </Typography>
+            <Box justifyContent="center">
+              <Rating
+                readOnly
+                max={3}
+                defaultValue={mapDiff(difficulty)}
+                precision={1}
+                emptyIcon={<StarBorder fontSize="inherit" />}
+              />
+            </Box>
+            <Button
+              autoFocus
+              color="inherit"
+              onClick={() => {
+                console.log("openFeedback", openFeedback);
+                setOpenFeedback(true);
+              }}
+            >
+              Submit
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Typography variant="h3" align="center">
+          {problem}
+        </Typography>
+        <Box alignSelf="center" mt={3}>
+          <RadioGroup
+            value={selected}
+            onChange={(e) => {
+              console.log(selected);
+              setSelected(parseInt(e.target.value));
+            }}
           >
-            <Close />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            {name}
-          </Typography>
-          <Box justifyContent="center">
-            <Rating
-              readOnly
-              max={3}
-              defaultValue={mapDiff(difficulty)}
-              precision={1}
-              emptyIcon={<StarBorder fontSize="inherit" />}
-            />
-          </Box>
-          <Button autoFocus color="inherit" onClick={handleClose}>
-            Submit
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Typography variant="h3" align="center">
-        {problem}
-      </Typography>
-      <Box alignSelf="center" mt={3}>
-        <RadioGroup
-          value={selected}
-          onChange={(e) => {
-            console.log(selected);
-            setSelected(parseInt(e.target.value));
-          }}
-        >
-          {solutions?.map((solution, i) => (
-            <FormControlLabel
-              key={i}
-              value={i}
-              label={solution}
-              control={<Radio />}
-            />
-          ))}
-        </RadioGroup>
-      </Box>
-    </Dialog>
+            {solutions?.map((solution, i) => (
+              <FormControlLabel
+                key={i}
+                value={i}
+                label={solution}
+                control={<Radio />}
+              />
+            ))}
+          </RadioGroup>
+        </Box>
+      </Dialog>
+      <Feedback open={openFeedback} setOpen={setOpenFeedback} />
+    </>
   );
 };
 
